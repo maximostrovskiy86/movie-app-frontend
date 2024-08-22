f5abe9a9f79c65e4eb5f22ba023d63bd908e8605
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-08-22 17:39:43 +0200

    Add-style-for-header

--- src/js/API/movie-api.js
+++ src/js/API/movie-api.js
@@ -1,32 +1,32 @@
-import {BASE_URL, API_KEY} from "../const/base";
+import { BASE_URL, API_KEY } from '../const/base';
 
 export const fetchGetMoviesTrending = async () => {
-    const params = new URLSearchParams({
-        'api_key': API_KEY,
-    });
+  const params = new URLSearchParams({
+    'api_key': API_KEY,
+  });
 
-    const response = await fetch(`${BASE_URL}/trending/movie/day?${params}`);
-    return await response.json();
-}
+  const response = await fetch(`${BASE_URL}/trending/movie/day?${params}`);
+  return await response.json();
+};
 
 export const fetchMoviesSearch = async (keyword) => {
-    const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=true&language=en-US&page=1`, {
-        // const response =   await fetch(`${BASE_URL}/trending/movie/day?${API_KEY}`, {
-        headers: {
-            Accept: "application/json",
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWMwOTAyNWI0YTUwMDMwN2FlMjZjODkzZjM5YzMzNyIsIm5iZiI6MTcyMjg1NzQ4MS44NDMyNzMsInN1YiI6IjYzNjNmMTBlMDkxZTYyMDA3YTFhZWE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BxHYf2Poplzh_y0ABnudvQzWstb3okQnNb4vYG6XQfA',
-        }
-    })
+  const response = await fetch(`https://api.themoviedb.org/3/search/movie?query=${keyword}&include_adult=true&language=en-US&page=1`, {
+    // const response =   await fetch(`${BASE_URL}/trending/movie/day?${API_KEY}`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWMwOTAyNWI0YTUwMDMwN2FlMjZjODkzZjM5YzMzNyIsIm5iZiI6MTcyMjg1NzQ4MS44NDMyNzMsInN1YiI6IjYzNjNmMTBlMDkxZTYyMDA3YTFhZWE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BxHYf2Poplzh_y0ABnudvQzWstb3okQnNb4vYG6XQfA',
+    },
+  });
 
-    return await response.json();
-}
+  return await response.json();
+};
 
 export const fetchMovieInformation = async (movie_id) => {
-    const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}`, {
-        headers: {
-            Accept: 'application/json',
-            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWMwOTAyNWI0YTUwMDMwN2FlMjZjODkzZjM5YzMzNyIsIm5iZiI6MTcyMzE5NjAyMS40Mzg5ODgsInN1YiI6IjYzNjNmMTBlMDkxZTYyMDA3YTFhZWE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o6ZEIP1NzHbPrgJMGw8q_yz4T_kEDzJwqFcOFkHlv8A',
-        }
-    })
-    return await response.json();
-}
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}`, {
+    headers: {
+      Accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MWMwOTAyNWI0YTUwMDMwN2FlMjZjODkzZjM5YzMzNyIsIm5iZiI6MTcyMzE5NjAyMS40Mzg5ODgsInN1YiI6IjYzNjNmMTBlMDkxZTYyMDA3YTFhZWE4OSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.o6ZEIP1NzHbPrgJMGw8q_yz4T_kEDzJwqFcOFkHlv8A',
+    },
+  });
+  return await response.json();
+};
